f9dd6e112ed6b7ae8e4453c23287d61395a8ba43
zebbo <kepaknaga@gmail.com>
2016-07-22 15:36:06 +0800

    letak input dan save ke variable

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 keyall = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#','A']
 
-for k in keyall[:2]:
+keyalln = ['0 =A','1 =A#','2 =B','3 =C','4 =C#','5 =D','6 =D#','7 =E','8 =F','9 =F#','10=G','11=G#']
+
+#for k in keyall[:12]:
+#	print(k)
+
+for k in keyalln :
 	print(k)
+
+
+keymula = input('starting key?= ')
+nomborMulaKey = int(keymula)
+
+print(nomborMulaKey)
+
+print(keyall[nomborMulaKey])
